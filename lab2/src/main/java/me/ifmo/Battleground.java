28e3d7888e09890a693317a729fb1a0ce619f60c
mysticslippers <eternal.martyr13579@gmail.com>
2023-07-02 17:42:39 +0300

    Готовая лабораторная работа №2.

--- lab2/src/main/java/me/ifmo/Battleground.java
+++ lab2/src/main/java/me/ifmo/Battleground.java
@@ -1,8 +1,26 @@
 package me.ifmo;
 
+import me.ifmo.pokemons.*;
 import ru.ifmo.se.pokemon.*;
 public class Battleground {
     public static void main(String[] args){
         Battle battle = new Battle();
+
+        Oddish oddish = new Oddish("Невестка", 10);
+        Gloom gloom = new Gloom("Свёкор", 10);
+        Bellossom bellossom = new Bellossom("Свекровь", 10);
+        Wishiwashi wishiwashi = new Wishiwashi("Зять", 10);
+        Bergmite bergmite = new Bergmite("Тёща", 10);
+        Avalugg avalugg = new Avalugg("Тесть", 10);
+
+        battle.addAlly(wishiwashi);
+        battle.addAlly(bergmite);
+        battle.addAlly(bellossom);
+
+        battle.addFoe(oddish);
+        battle.addFoe(gloom);
+        battle.addFoe(avalugg);
+
+        battle.go();
     }
 }
